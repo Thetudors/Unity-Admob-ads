0b17971620d661fcad5ebc106cb68d92d0dfd734
Onat <152804004@ogr.cbu.edu.tr>
2017-08-12 02:30:15 +0300

    Unity admob ads init

---
+++ Assets/Ad_Manager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using admob;
+public class Ad_Manager : MonoBehaviour {
+
+	public static Ad_Manager Instance { get; set; }
+    public string Banner_id;
+    public string interstitial_id;
+
+    
+
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        
+
+      
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+
+        Admob.Instance().initAdmob(Banner_id,interstitial_id);
+       // Admob.Instance().loadRewardedVideo("ca-app-pub-3800355237509207/1762562834");
+        Admob.Instance().setTesting(true); //Test amaçlı reklamlar
+        Admob.Instance().loadInterstitial(); //Geçiş reklamı yüklemi.
+#endif
+
+    }
+
+    public void Show_Banner_top()
+    {
+      
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+
+        Admob.Instance().showBannerRelative(AdSize.SmartBanner, AdPosition.TOP_CENTER,0);
+#endif
+    }
+    public void Show_Banner_bot()
+    {
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+
+        Admob.Instance().showBannerRelative(AdSize.SmartBanner, AdPosition.BOTTOM_CENTER,0);
+#endif
+    }
+
+    public void Show_Video()
+    {
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+
+        if (Admob.Instance().isInterstitialReady())
+        {
+            Admob.Instance().showInterstitial();
+            Admob.Instance().loadInterstitial();
+        }
+#endif
+
+    }
+
+    public void Remove_Banner()
+    {
+
+        
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+        Admob.Instance().removeAllBanner();
+#endif
+    }
+
+    public void Show_Rewarted()
+    {
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+        if(Admob.Instance().isRewardedVideoReady())
+        {
+            Admob.Instance().showRewardedVideo();
+            
+        }
+#endif
+
+    }
+}
---
+++ Assets/Admob_Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using admob;
+
+public class Admob_Manager : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+       
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Banner_Click()    
+    {
+
+        FindObjectOfType<Ad_Manager>().Show_Banner_top();
+
+    }
+    public void Gecis_Click()
+    {
+        FindObjectOfType<Ad_Manager>().Show_Video();
+    }
+
+    }
